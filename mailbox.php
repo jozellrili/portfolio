83726ab0f9097b14897fb66034c16fd11af226d7
jozellrili <jozellrili@gmail.com>
2016-06-30 19:56:06 +0800

    remove alert box and replaced by header location

--- mailbox.php
+++ mailbox.php
@@ -5,13 +5,7 @@ include('test_input.php');
 if(!empty($fname) && !empty($lname) && !empty($email) && !empty($message)) {
 	$sql = ("INSERT INTO mailbox (fname, lname, email, phone, message) VALUES ('$fname', '$lname', '$email', '$phone', '$message')");
 	mysqli_query($con, $sql);
-}
-else {
-	echo '
-		<script type="text/javascript">
-			alert("fill all the required fields!");
-		</script>
-	';
+	header('Location: index.php');
 }
 
 ?>
