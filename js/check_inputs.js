045e74423f6285ed3889e0c6986c77c8534abd0a
jozellrili <jozellrili@gmail.com>
2016-06-30 19:03:54 +0800

    script for disabling button in required fields is empty

---
+++ js/check_inputs.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+    validate();
+    $('#form_name, #form_lastname, #form_email, #form_phone, #form_message').change(validate);
+});
+
+function validate(){
+    if ($('#form_name').val().length   >   0   &&
+        $('#form_lastname').val().length  >   0   &&
+        $('#form_email').val().length    >   0 &&
+        $('#form_phone').val().length > 0  &&
+        $('#form_message').val().length > 0 )  {
+        $("button[type=submit]").prop("disabled", false);
+    }
+    else {
+        $("button[type=submit]").prop("disabled", true);
+    }
+}
