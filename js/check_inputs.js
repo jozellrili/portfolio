5ae8f7403b17a199df49a75ecd33f6c901d8b16b
jozellrili <jozellrili@gmail.com>
2016-06-30 19:53:12 +0800

    remove #form_phone from check_inputs

--- js/check_inputs.js
+++ js/check_inputs.js
@@ -1,13 +1,12 @@
 $(document).ready(function (){
     validate();
-    $('#form_name, #form_lastname, #form_email, #form_phone, #form_message').change(validate);
+    $('#form_name, #form_lastname, #form_email, #form_message').change(validate);
 });
 
 function validate(){
     if ($('#form_name').val().length   >   0   &&
         $('#form_lastname').val().length  >   0   &&
         $('#form_email').val().length    >   0 &&
-        $('#form_phone').val().length > 0  &&
         $('#form_message').val().length > 0 )  {
         $("button[type=submit]").prop("disabled", false);
     }
